aaa140645ad8fd6dbde7ec5eaf40bab09bb8e7c2
Sam Hunt <samuelhunt404@gmail.com>
2025-11-20 10:15:29 -0700

    first commit

---
+++ EnvHandler.gd
@@ -0,0 +1,7 @@
+@tool
+extends WorldEnvironment
+
+#@export var windSpeed : Vector2 = Vector2(0.01,0.0)
+#func _process(delta: float) -> void:
+	#var windOff : Vector2 = RenderingServer.global_shader_parameter_get("WindOff")
+	#RenderingServer.global_shader_parameter_set("WindOff",windOff + windSpeed)
